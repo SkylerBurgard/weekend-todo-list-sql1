b1986ce7922589b4c349217438bc191d2a9a6b6b
Skyler Burgard <skylerrburgard@hotmail.com>
2020-07-11 16:15:27 -0500

    Workign on GET/POST in client.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,2 +1,38 @@
-$(document).ready(onReady);
-console.log('JQ');
+$(document).ready(init);
+
+function init() {
+    console.log('Ready to go');
+    $('#js-btn-task').on('click', addTask);
+};
+
+function addTask() {
+    const task = $('.taskIn').val();
+    let completed = $('.#')
+}
+
+function getTask() {
+    $.ajax({
+            type: 'GET',
+            url: '/toDo'
+        })
+        .then((dbResponse) => {
+            console.log(dbResponse);
+            render(dbResponse);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+function postTask(objectToSend) {
+    $.ajax({
+            method: 'POST',
+            url: '/list',
+            data: objectToSend,
+        })
+        .then(function (err) {
+            console.log(err);
+        });
+}
+
+function render(tasks) {}
