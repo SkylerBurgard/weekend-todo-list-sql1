acf30c5d9178f98346611759ac035650f3176842
Skyler Burgard <skylerrburgard@hotmail.com>
2020-07-12 13:32:33 -0500

    Setting up event lsiteners and the cooresponding functions with their routes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,14 +1,31 @@
 $(document).ready(init);
 
 function init() {
+    getTask();
     console.log('Ready to go');
     $('#js-btn-task').on('click', addTask);
-};
+    $('#viewTasks').on('click', '#js-btn-delete', deleteTask);
+    ('#viewTasks').on('click', '#js-btn-update', completeTask);
+    ('#completedTask').on('click', '#js-btn-delete', deleteTask);
+}; // setting click event.. 
+
+//create functions to call event listeners
 
 function addTask() {
     const task = $('.js-taskIn').val();
-    let completed = $('.#')
-}
+    let completed = $('.#completedIn').val();
+
+    let taskToSend = {
+        task,
+        completed,
+    }
+    // console.log(taskToSend);
+
+    postTask(taskToSend);
+    $('#taskIn').val('');
+    $('#completedIn').val('');
+}; //posting to homepage and setting our values to empty string
+
 
 function getTask() {
     $.ajax({
@@ -17,22 +34,40 @@ function getTask() {
         })
         .then((dbResponse) => {
             console.log(dbResponse);
-            render(dbResponse);
+            render(tasks);
         })
         .catch((error) => {
             console.log(error);
         });
 }
 
-function postTask(objectToSend) {
+function postTask(tasksToSend) {
     $.ajax({
             method: 'POST',
             url: '/list',
             data: objectToSend,
         })
-        .then(function (err) {
-            console.log(err);
+        .then(function (dbResponse) {
+            console.log(dbResponse);
         });
+    getTask(dbResponse);
+};
+
+function deleteTask() {
+    const id =
+        $.ajax({
+            type: 'DELETE',
+            url: '/toDo/delete' + id
+        })
+        .then(function (dbResponse) {
+            getTask();
+        })
+
 }
 
-function render(tasks) {}
+function completeTask() {
+
+
+
+
+    function render(tasks) {}
